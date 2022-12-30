d7c076c367d020e4abf38b6720ede93f7594031c
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-12-30 23:31:36 +0100

    reset position of images

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import IconDice from '../public/images/icon-dice.svg'
-import PatternDivider from '../public/images/pattern-divider-desktop.svg'
+import IconDice from './images/icon-dice.svg'
+import PatternDivider from './images/pattern-divider-desktop.svg'
+
+// Interface declaration for the recieved data from fetch
+type Data = {
+    slip: {
+        id: number,
+        advice: string
+    }
+}
 
 
 const App: React.FunctionComponent = () => {
+
+    const [data, setData] = useState<Data>()
+
+    const randomQuote = () => {
+        fetch('https://api.adviceslip.com/advice')
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        randomQuote()
+        // console.log(data)
+    }, [])
+
+
+
+
+
     return (
         <div className='container'>
-            <div className='advice-card'>
-                <div className='card-content'>
-                    <p>Advice #117</p>
-                    <h1>
-                        "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-                    </h1>
-                    <div className='section-divider'>
-                        <img src={PatternDivider} alt="pattern-divider" />
+            {
+                data && (
+                    <div className='advice-card'>
+                        <div className='card-content'>
+                            <p>Advice #{data.slip.id}</p>
+                            <h1>
+                                "{data.slip.advice}."
+                            </h1>
+                            <div className='section-divider'>
+                                <img src={PatternDivider} alt="pattern-divider" />
+                            </div>
+                        </div>
+                        <div className='button-container'>
+                            <button className='random-button' onClick={randomQuote}>
+                                <img src={IconDice} alt="dice-icon" />
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div className='button-container'>
-                    <button className='random-button'>
-                        <img src={IconDice} alt="dice-icon" />
-                    </button>
-                </div>
-            </div>
+                )
+            }
         </div>
     )
 }
